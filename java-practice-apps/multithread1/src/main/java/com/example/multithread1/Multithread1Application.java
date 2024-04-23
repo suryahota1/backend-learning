3f66b13113c2a173d71e7174be83b52943f521a5
Suryakant Hota <suryakant.hota2@gmail.com>
2024-04-23 10:07:45 +0530

    initial commit

---
+++ java-practice-apps/multithread1/src/main/java/com/example/multithread1/Multithread1Application.java
@@ -0,0 +1,13 @@
+package com.example.multithread1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Multithread1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Multithread1Application.class, args);
+	}
+
+}
